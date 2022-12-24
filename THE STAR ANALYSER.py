1aa1e09de9642e83ebc77428aeaedc6764789e9d
Willy-Wonko <aidar1islamov@gmail.com>
2022-12-24 21:01:54 +0300

    уменьшил код

--- THE STAR ANALYSER.py
+++ THE STAR ANALYSER.py
@@ -7,203 +7,46 @@ from tkinter import messagebox
 import math as m
 
 ##################################################### Графики
-def plot_verticles(vertices, isosurf = False, filename = None, colour = None, cmap_ = None):
-    # Create a new plot
+def graph_3D(colour=None, cmap_='BuPu_r'):
+    points = []
+    for i in range(-R, R):
+        for j in range(-R, R):
+            points.append([x + i, y + j, data[y + j][x + i]])
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    x = [v[0] for v in vertices]
-    y = [v[1] for v in vertices]
-    z = [v[2] for v in vertices]
-    if isosurf:
-        ax.plot_trisurf(x, y, z, linewidth=0.2, antialiased=True, color=colour, cmap = cmap_)
-    else:
-        ax.scatter(x, y, z, c='r', marker='o')
-    # ax.set_xlabel('X')
-    # ax.set_ylabel('Y')
-    # ax.set_zlabel('Z')
-    # Show or save the plot
-    if filename is None:
-        plt.show()
-    else:
-        plt.savefig(filename)
+    x_p = [p[0] for p in points]
+    y_p = [p[1] for p in points]
+    z_p = [p[2] for p in points]
+    ax.plot_trisurf(x_p, y_p, z_p, linewidth=0.2, antialiased=True, color=colour, cmap = cmap_)
+    plt.show()
 
 def profile(xp, yp, type):
     global data, R
-    srez = []
-    data_vrem = []
     if type == "horisontal":
-        plt.plot(data[yp][(xp-R):(xp+R)])
+        abciss = list(range(xp-R, xp+R))
+        plt.plot(abciss, data[yp][(xp-R):(xp+R)])
         plt.title("Горизонтальный профиль")
         plt.show()
     if type == "vertical":
-        data_vrem = np.transpose(data)
-        plt.plot(data_vrem[xp][(yp-R):(yp+R)])
+        abciss = list(range(yp - R, yp + R))
+        data_vrem = data.T
+        plt.plot(abciss, data_vrem[xp][(yp-R):(yp+R)])
         plt.title("Вертикальный профиль")
         plt.show()
+# #Reds Blues_r BrBG_r BuPu_r
 
-    #
-    # for i in range(-R, R):
-    #     srez.append(0)
-    #     srez[i+R] = data_vrem[yp][xp+i]
-    # abciss = []
-    # for i in range(xp - R, xp + R):
-    #     abciss.append(i)
-
-def profile_x():
-    horisont = []
-    for i in range(0, R):
-        horisont.append(0)
-        horisont[i] = data[y][x - (R-i)]
-    for i in range(0, R):
-        horisont.append(0)
-        horisont[R + i] = data[y][x + i]
-    abciss = []
-    for i in range(y-R, y+R):
-        abciss.append(i)
-
-    plt.plot(abciss, horisont)
-    plt.title("Горизонтальный профиль")
-    plt.show()
-
-def profile_y():
-    vertikal = []
-    for i in range(0, R):
-        vertikal.append(0)
-        vertikal[i] = data[y - (R-i)][x]
-    for i in range(0, R):
-        vertikal.append(0)
-        vertikal[R + i] = data[y + i][x]
-    ordinan = []
-    for i in range(x-R, x+R):
-        ordinan.append(i)
-
-    plt.plot(ordinan, vertikal)
-    plt.title("Вертикальный профиль")
-    plt.show()
-
-""" жалко удалять
-def graph_3D_tochecny():
-    ax = plt.axes(projection='3d')
-    for j in range(0, R):
-
-        coord_sreza = []
-        srez = []
-        for i in range(0, R + 1):
-            coord_sreza.append(1)
-            coord_sreza[i] = y - (R - i)
-        for i in range(1, R + 1):
-            coord_sreza.append(1)
-            coord_sreza[R + i] = y + i
-        print(coord_sreza)
-
-        for i in range(0, R + 1):
-            srez.append(1)
-            srez[i] = data[y - (R - i)][x - (R - j)]
-        for i in range(1, R + 1):
-            srez.append(1)
-
-        ax.scatter3D(x - (R - j), coord_sreza, srez, color="red")
-
-    for j in range(0, R):
-
-        coord_sreza = []
-        srez = []
-        for i in range(0, R + 1):
-            coord_sreza.append(1)
-            coord_sreza[i] = y - (R - i)
-        for i in range(1, R + 1):
-            coord_sreza.append(1)
-            coord_sreza[R + i] = y + i
-        print(coord_sreza)
-
-        for i in range(0, R + 1):
-            srez.append(1)
-            srez[i] = data[y - (R - i)][x + j]
-        for i in range(1, R + 1):
-            srez.append(1)
-            srez[R + i] = data[y + i][x + j]
-        print("srez:", srez)
-
-        ax.scatter3D(x + j, coord_sreza, srez, color="red")
-"""
-def graph_3D():
-    gr = []
-    for i in range(-R, R):
-        for j in range(-R, R):
-            gr.append([x + i, y + j, data[y + j][x + i]])
-    plot_verticles(vertices=gr, isosurf=True, cmap_="BuPu_r")
-    # #Reds Blues_r BrBG_r BuPu_r
 ####################################################### Сумма отсчетов
-"""
-def summing_staroe():
-    global sum_otchetov
-    ############# Вычисление среднего потока фона
-    sum_fona = 0
-    n_fona = 0
-    sum_zvezda_istinnoe = 0
-
-    for j in range(0, R_vnesh):
-        for i in range (0, R_vnesh):
-            if m.sqrt(i**2+j**2)<=R_vnesh and m.sqrt(i**2+j**2)>= R: #расстояние пикселя от центра звезды меньше R fona и больше R:
-                n_fona += 1
-                sum_fona += data[y-(R_vnesh-j)][x-(R-j)]
-        for i in range (1, R_vnesh):
-            if m.sqrt(i**2+j**2)<=R_vnesh and m.sqrt(i**2+j**2)>= R:
-                n_fona += 1
-                sum_fona += data[y + j][x-(R-j)]
-
-
-    for j in range(1, R_vnesh+1):
-        for i in range (0, R_vnesh):
-            if m.sqrt(i**2+j**2)<=R_vnesh and m.sqrt(i**2+j**2)>= R:
-                n_fona += 1
-                sum_fona += data[y-(R_vnesh-j)][x+j]
-        for i in range (1, R_vnesh):
-            if m.sqrt(i**2+j**2)<=R_vnesh and m.sqrt(i**2+j**2)>= R:
-                n_fona += 1
-                sum_fona += data[y + j][x+j]
-
-
-    srednee_fona = sum_fona/n_fona
-
-    ############## Вычисление суммарного потока звезды
-    n_zvezda = 0
-    sum_zvezda = 0
-    for j in range(0, r):
-        for i in range(0, r):
-            if  m.sqrt(i**2+j**2)<=r: # расстояние пикселя от центра звезды меньше r:
-                n_zvezda += 1
-                sum_zvezda += data[y - (r - j)][x - (r - j)]
-        for i in range(1, r):
-            if  m.sqrt(i**2+j**2)<=r:
-                n_zvezda += 1
-                sum_zvezda += data[y + j][x - (r - j)]
-
-    for j in range(1, r + 1):
-        for i in range(0, r):
-            if  m.sqrt(i**2+j**2)<=r:
-                n_zvezda += 1
-                sum_zvezda += data[y - (r - j)][x + j]
-        for i in range(1, r):
-            if  m.sqrt(i**2+j**2)<=r:
-                n_zvezda += 1
-                sum_zvezda += data[y + j][x + j]
-    sum_zvezda_istinnoe = sum_zvezda - n_zvezda*srednee_fona
-    sum_otchetov = sum_zvezda_istinnoe
-    #print("Истинная сумма отсчетов звезды=", int(sum_zvezda_istinnoe))
-"""
-
 def summing():
     global sum_otchetov
     ############# Вычисление среднего потока фона
     sum_fona = 0
     n_fona = 0
 
-    for j in range(-R_vnesh, R_vnesh):
-        for i in range(-R_vnesh, R_vnesh):
+    for i in range(-R_vnesh, R_vnesh):
+        for j in range(-R_vnesh, R_vnesh):
             if m.sqrt(i**2+j**2)<=R_vnesh and m.sqrt(i**2+j**2)>R:  # расстояние пикселя от центра звезды меньше R fona и больше R:
                 n_fona += 1
-                sum_fona += data[y + j][x + j]
+                sum_fona += data[y + j][x + i]
 
     srednee_fona = sum_fona / n_fona
 
@@ -216,24 +59,23 @@ def summing():
                 n_zvezda += 1
                 sum_zvezda += data[y + j][x + j]
 
-    sum_zvezda_istinnoe = 0
     sum_zvezda_istinnoe = sum_zvezda - n_zvezda * srednee_fona
     sum_otchetov = sum_zvezda_istinnoe
 
 #################################################################################   Command OK
-
 def ok():
     global x, y, r, R, R_vnesh, file, data
-
-    snimok = pf.open(txt.get(1.0, END).replace("\n", ""))
-    data = snimok[0].data
-    snimok.close()
-
-    x = int(x_coord.get(1.0, END))
-    y = int(y_coord.get(1.0, END))
-    r = int(mesto_vvoda_r.get(1.0, END))
-    R = int(mesto_vvoda_R.get(1.0, END))
-    R_vnesh = int(mesto_vvoda_R_vnesh.get(1.0, END))
+    try:
+        snimok = pf.open(txt.get(1.0, END).replace("\n", ""))
+        data = snimok[0].data
+        snimok.close()
+        x = int(x_coord.get(1.0, END))
+        y = int(y_coord.get(1.0, END))
+        r = int(mesto_vvoda_r.get(1.0, END))
+        R = int(mesto_vvoda_R.get(1.0, END))
+        R_vnesh = int(mesto_vvoda_R_vnesh.get(1.0, END))
+    except BaseException as e:
+        messagebox.showinfo("Ошибка", f"Недопустимый ввод данных\n\n{e} ")
     global sum_otchetov
     if chk_X_state.get():
         profile(x, y, "horisontal")
@@ -245,13 +87,9 @@ def ok():
         summing()
         messagebox.showinfo('Cумма отсчетов', int(sum_otchetov)) # как задать размеры вспл окна?
 ########################################## Открываем окно
-
 window = Tk()
 window.title("THE STAR")
 window.geometry('600x300')
-
-
-
 #############################################################################     Вводные данные
 
 lbl_file = Label(window, text="Путь к файлу:", font=("Arial Bold", 10))
@@ -327,3 +165,71 @@ btn_ok.grid(column=2, row=7)
 
 ################################################################# конец
 window.mainloop()
+
+""""
+def plot_verticles(vertices, isosurf = False, filename = None, colour = None, cmap_ = None):
+    # Create a new plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    x = [v[0] for v in vertices]
+    y = [v[1] for v in vertices]
+    z = [v[2] for v in vertices]
+    if isosurf:
+        ax.plot_trisurf(x, y, z, linewidth=0.2, antialiased=True, color=colour, cmap = cmap_)
+    else:
+        ax.scatter(x, y, z, c='r', marker='o')
+    # ax.set_xlabel('X')
+    # ax.set_ylabel('Y')
+    # ax.set_zlabel('Z')
+    # Show or save the plot
+    if filename is None:
+        plt.show()
+    else:
+        plt.savefig(filename)
+"""
+
+""" жалко удалять
+def graph_3D_tochecny():
+    ax = plt.axes(projection='3d')
+    for j in range(0, R):
+
+        coord_sreza = []
+        srez = []
+        for i in range(0, R + 1):
+            coord_sreza.append(1)
+            coord_sreza[i] = y - (R - i)
+        for i in range(1, R + 1):
+            coord_sreza.append(1)
+            coord_sreza[R + i] = y + i
+        print(coord_sreza)
+
+        for i in range(0, R + 1):
+            srez.append(1)
+            srez[i] = data[y - (R - i)][x - (R - j)]
+        for i in range(1, R + 1):
+            srez.append(1)
+
+        ax.scatter3D(x - (R - j), coord_sreza, srez, color="red")
+
+    for j in range(0, R):
+
+        coord_sreza = []
+        srez = []
+        for i in range(0, R + 1):
+            coord_sreza.append(1)
+            coord_sreza[i] = y - (R - i)
+        for i in range(1, R + 1):
+            coord_sreza.append(1)
+            coord_sreza[R + i] = y + i
+        print(coord_sreza)
+
+        for i in range(0, R + 1):
+            srez.append(1)
+            srez[i] = data[y - (R - i)][x + j]
+        for i in range(1, R + 1):
+            srez.append(1)
+            srez[R + i] = data[y + i][x + j]
+        print("srez:", srez)
+
+        ax.scatter3D(x + j, coord_sreza, srez, color="red")
+"""
